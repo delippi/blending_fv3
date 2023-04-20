39033f28a6b0d0e910c4d7356df0981df6d6ac38
Donald.E.Lippi <donald.e.lippi@noaa.gov>
2023-04-20 18:34:47 +0000

    initial commit of code

---
+++ python/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#f2py3.9 --build-dir . -c raymond.f -m raymond -DF2PY_REPORT_ON_ARRAY_COPY=1
+
+conda activate
+f2py3.9 --build-dir . -c -m raymond raymond.f -DF2PY_REPORT_ON_ARRAY_COPY=1
