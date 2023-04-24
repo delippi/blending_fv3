ed3097cb5e7568f626205bd7ff8118eb7551c443
Donald.E.Lippi <donald.e.lippi@noaa.gov>
2023-04-24 16:38:10 +0000

    update scripts and paths for wcoss2 branch

--- chgres/regrid_GDAS_2_RRFS.sh
+++ chgres/regrid_GDAS_2_RRFS.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-##SBATCH --ntasks=60
-#SBATCH --nodes=3
-#SBATCH --ntasks-per-node=20
-#SBATCH -t 0:40:00
-#SBATCH -A hfv3gfs
-#SBATCH -J blending
+#PBS -N blending
+#PBS -A RRFS-DEV
+#PBS -q dev
+##PBS -l nodes=3:ppn=20
+#PBS -l place=vscatter,select=2:ncpus=32
+#PBS -l walltime=0:40:00
+#PBS -j oe
+
+source /lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs/ufs-srweather-app/env/build_wcoss2_intel.env
 
 module list
 
 ulimit -s unlimited
-cd /mnt/lfs1/data_untrusted/Donald.E.Lippi/blending/fv3/chgres
-srun ./chgres_cube
+export OMP_STACKSIZE=1G
+export OMP_NUM_THREADS="4"
+export FI_OFI_RXM_SAR_LIMIT=3145728
+export FI_MR_CACHE_MAX_COUNT=0
+export MPICH_OFI_STARTUP_CONNECT=1
+export NNODES_MAKE_ICS="2"
+export PPN_MAKE_ICS="32"
+ncores=$(( NNODES_MAKE_ICS*PPN_MAKE_ICS ))
+APRUN="mpiexec -n ${ncores} -ppn ${PPN_MAKE_ICS} --cpu-bind core --depth ${OMP_NUM_THREADS}"
+
+cd /lfs/h2/emc/da/noscrub/donald.e.lippi/blending/blending_fv3/chgres
+$APRUN ./chgres_cube
