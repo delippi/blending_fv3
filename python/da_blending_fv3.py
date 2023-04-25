339733dbb198eda3a319c58bb93543d91134974e
Donald.E.Lippi <donald.e.lippi@noaa.gov>
2023-04-25 14:38:40 +0000

    update for pep8 coding standards.

--- python/da_blending_fv3.py
+++ python/da_blending_fv3.py
@@ -5,7 +5,6 @@ import os as _os
 import emcpy.utils.dateutils as _dateutils
 from netCDF4 import Dataset
 import raymond
-#import balance
 import shutil
 import pdb
 
@@ -13,14 +12,15 @@ model = "RRFS"
 host = "GDAS"
 Lx = 960.0
 pi = np.pi
-#variables = ["U", "V", "T", "QVAPOR", "PH", "P", "MU", "U10", "V10", "T2", "Q2", "PSFC", "TH2"]
-vars_fg = ["u",     "v", "T"]
-vars_bg = ["u_s", "v_w", "t"]
+
+# Eventually, these two lists should be the same. There is still some technical work
+# to be done to make sure the chgres_cube output (coldstart) matches the same grid
+# staggering as the RRFS restart (warm start).
+vars_fg = ["u", "v", "T", "sphum", "delp"]
+vars_bg = ["u_s", "v_w", "t", "sphum", "delp"]
 
 nbdy = 40  # 20 on each side
 blend = True
-diagnose = False
-debug = False
 
 if blend:
     print("Starting blending")
@@ -34,10 +34,9 @@ if blend:
     glb_Dx = 3.0
 
 #   RRFS EnKF restart file on ESG grid.
-    if not debug:
-        print("copying file...")
-        shutil.copyfile("./fg","./fg_blend.nc")
-        print("copy done.")
+    print("copying file...")
+    shutil.copyfile("./fg", "./fg_blend.nc")
+    print("copy done.")
     reg_fg = "./fg_blend.nc"
     # Open the blended file for updating the required vars (use a copy of the regional file)
     reg_fg_nc = Dataset(reg_fg, mode="a")
@@ -46,7 +45,6 @@ if blend:
     nlat = reg_fg_nc.dimensions["yaxis_2"].size  # 1092   (yaxis_1=1093)
     nlev = reg_fg_nc.dimensions["zaxis_1"].size  # 65
     Dx = 3.0
-    #Dx = reg_fg_nc.dx / 1000.0
 
     # Check matching grids
     if (glb_nlon != nlon or glb_nlat != nlat or glb_nlev != nlev or glb_Dx != Dx):
@@ -79,56 +77,56 @@ if blend:
         print(f"Blending backgrounds for {var_fg}/{var_bg}")
 
         dim = len(np.shape(reg_fg_nc[var_fg]))-1
-        if dim == 2:  #2D vars
-           glb = np.float64(glb_fg_nc[var_bg][:,:])  # (1093 1820)
-           reg = np.float64(reg_fg_nc[var_fg][:,:,:])  # (1, 1093, 1820)
-           ntim = np.shape(reg)[0]
-           nlat = np.shape(reg)[1]
-           nlon = np.shape(reg)[2]
-           nlev = 1
-           glb = np.reshape(glb, [ntim, nlat, nlon])  # add time dim bc missing from chgres
-           var_out = np.zeros(shape=(nlon, nlat, 1), dtype=np.float64)
-           field = np.zeros(shape=(nlon*nlat), dtype=np.float64)
-           var_work = np.zeros(shape=((nlon+nbdy), (nlat+nbdy), 1), dtype=np.float64)
-           field_work = np.zeros(shape=((nlon+nbdy)*(nlat+nbdy)), dtype=np.float64)
-        if dim == 3:  #3D vars
-           glb = np.float64(glb_fg_nc[var_bg][:,:,:])       # (65, 1093, 1820)
-           reg = np.float64(reg_fg_nc[var_fg][:,:,:,:])  # (1, 65, 1093, 1820)
-           ntim = np.shape(reg)[0]
-           nlev = np.shape(reg)[1]
-           nlat = np.shape(reg)[2]
-           nlon = np.shape(reg)[3]
-           glb = np.reshape(glb, [ntim, nlev, nlat, nlon])  # add time dim bc missing form chgres
-           var_out = np.zeros(shape=(nlon, nlat, nlev, 1), dtype=np.float64)
-           field = np.zeros(shape=(nlon*nlat, nlev), dtype=np.float64)
-           var_work = np.zeros(shape=((nlon+nbdy), (nlat+nbdy),nlev, 1), dtype=np.float64)
-           field_work = np.zeros(shape=((nlon+nbdy)*(nlat+nbdy),nlev), dtype=np.float64)
+        if dim == 2:  # 2D vars
+            glb = np.float64(glb_fg_nc[var_bg][:, :])     # (1093 1820)
+            reg = np.float64(reg_fg_nc[var_fg][:, :, :])  # (1, 1093, 1820)
+            ntim = np.shape(reg)[0]
+            nlat = np.shape(reg)[1]
+            nlon = np.shape(reg)[2]
+            nlev = 1
+            glb = np.reshape(glb, [ntim, nlat, nlon])  # add time dim bc missing from chgres
+            var_out = np.zeros(shape=(nlon, nlat, 1), dtype=np.float64)
+            field = np.zeros(shape=(nlon*nlat), dtype=np.float64)
+            var_work = np.zeros(shape=((nlon+nbdy), (nlat+nbdy), 1), dtype=np.float64)
+            field_work = np.zeros(shape=((nlon+nbdy)*(nlat+nbdy)), dtype=np.float64)
+        if dim == 3:  # 3D vars
+            glb = np.float64(glb_fg_nc[var_bg][:, :, :])     # (65, 1093, 1820)
+            reg = np.float64(reg_fg_nc[var_fg][:, :, :, :])  # (1, 65, 1093, 1820)
+            ntim = np.shape(reg)[0]
+            nlev = np.shape(reg)[1]
+            nlat = np.shape(reg)[2]
+            nlon = np.shape(reg)[3]
+            glb = np.reshape(glb, [ntim, nlev, nlat, nlon])  # add time dim bc missing from chgres
+            var_out = np.zeros(shape=(nlon, nlat, nlev, 1), dtype=np.float64)
+            field = np.zeros(shape=(nlon*nlat, nlev), dtype=np.float64)
+            var_work = np.zeros(shape=((nlon+nbdy), (nlat+nbdy), nlev, 1), dtype=np.float64)
+            field_work = np.zeros(shape=((nlon+nbdy)*(nlat+nbdy), nlev), dtype=np.float64)
         glbT = np.transpose(glb)        # (1820, 1093, 65)
         regT = np.transpose(reg)        # (1820, 1093, 65, 1)
 
-        nlon_start=int(nbdy/2)
+        nlon_start = int(nbdy/2)
         nlon_end = int(nlon+nbdy/2)
-        nlat_start=int(nbdy/2)
+        nlat_start = int(nbdy/2)
         nlat_end = int(nlat+nbdy/2)
 
         var_work[nlon_start:nlon_end, nlat_start:nlat_end, :] = glbT - regT
-        field_work = var_work.reshape((nlon+nbdy)*(nlat+nbdy), nlev, order="F") #order="F" (FORTRAN)
+        field_work = var_work.reshape((nlon+nbdy)*(nlat+nbdy), nlev, order="F")  # order="F" (FORTRAN)
         field_work = raymond.raymond(field_work, nlon+nbdy, nlat+nbdy, eps, nlev)
         var_work = field_work.reshape(nlon+nbdy, nlat+nbdy, nlev, order="F")
         var_out = var_work[nlon_start:nlon_end, nlat_start:nlat_end, :]
-        if dim == 2:  #2D vars
-            var_out = var_out[:,:,0] + regT[:,:,0]
-            var_out = np.reshape(var_out,[nlon, nlat, 1])  # add the time ("1") dimension back
-        if dim == 3:  #3D vars
-            var_out = var_out + regT[:,:,:,0]
-            var_out = np.reshape(var_out,[nlon, nlat, nlev, 1])  # add the time ("1") dimension back
+        if dim == 2:  # 2D vars
+            var_out = var_out[:, :, 0] + regT[:, :, 0]
+            var_out = np.reshape(var_out, [nlon, nlat, 1])  # add the time ("1") dimension back
+        if dim == 3:  # 3D vars
+            var_out = var_out + regT[:, :, :, 0]
+            var_out = np.reshape(var_out, [nlon, nlat, nlev, 1])  # add the time ("1") dimension back
         var_out = np.transpose(var_out)  # (1, 50, 834, 954)
 
         # Overwrite blended fields to blended file.
-        if dim == 2:  #2D vars
-            reg_fg_nc.variables[var_fg][:,:,:] = var_out
-        if dim == 3:  #3D vars
-            reg_fg_nc.variables[var_fg][:,:,:,:] = var_out
+        if dim == 2:  # 2D vars
+            reg_fg_nc.variables[var_fg][:, :, :] = var_out
+        if dim == 3:  # 3D vars
+            reg_fg_nc.variables[var_fg][:, :, :, :] = var_out
 
     # Close nc files
     reg_fg_nc.close()  # blended file
