8c66d1c93367435093325d6ebe268b0d15f30a36
donald lippi <donald.e.lippi@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2023-06-20 18:28:11 +0000

    saving changes for running with RRFS

--- tmp_ICS/run.sh
+++ tmp_ICS/run.sh
@@ -5,24 +5,55 @@
 #PBS -l select=2:ncpus=128
 #PBS -A RRFS-DEV
 #PBS -q dev
-#PBS -l walltime=000:30:00
+#PBS -l walltime=000:45:00
 
-set -x
+ulimit -s unlimited
+export OMP_STACKSIZE=2G
+export OMP_NUM_THREADS="52"  #largest value tried that doesn't crash with fastest time =  (s)
+                             #1
+                             #2
+                             #4
+                             #13
+                             #26
+                             #52
+                             #364
+export FI_OFI_RXM_SAR_LIMIT=3145728
+export FI_MR_CACHE_MAX_COUNT=0
+export MPICH_OFI_STARTUP_CONNECT=1
+
+echo "OMP_STACKSIZE="$OMP_STACKSIZE
+echo "OMP_NUM_THREADS="$OMP_NUM_THREADS
+
+
 cd /lfs/h2/emc/da/noscrub/donald.e.lippi/blending/blending_fv3/tmp_ICS
-cp ./gdas/out.atm.tile7.nc .
-# Copy the RRFS restarts because they are overwritten
-cp ./rrfs/RESTART/20220720.180000.fv_core.res.tile1.nc ./fv_core.res.tile1.nc
-cp ./rrfs/RESTART/20220720.180000.fv_tracer.res.tile1.nc ./fv_tracer.res.tile1.nc
-cp ./rrfs/RESTART/20220720.180000.fv_core.res.nc ./fv_core.res.nc
 
-# Does not remap chgres winds to D-grid (blends u and u_s, which is not correct)
-#python ./da_blending_fv3.py out.atm.tile7.nc 20220720.180000.fv_core.res.tile1.nc 20220720.180000.fv_tracer.res.tile1.nc
+echo -ne "Copying files..............\r"
+# GDAS FILES
+cp ./gdas/out.atm.tile7.nc ./out.atm.tile7.nc
+cp ./gdas/gfs_ctrl.nc      ./gfs_ctrl.nc
+cp ./gdas/gfs.bndy.nc      ./gfs.bndy.nc
 
-# Does remap chgres winds to D-grid
-python ./da_blending_fv3_dgrid.py out.atm.tile7.nc fv_core.res.tile1.nc fv_tracer.res.tile1.nc fv_core.res.nc
+# RRFS FILES
+cp ./rrfs/RESTART/20220720.180000.fv_core.res.tile1.nc   ./fv_core.res.tile1.nc
+#cp ./rrfs/RESTART/20220720.180000.fv_tracer.res.tile1.nc ./fv_tracer.res.tile1.nc
+cp ./rrfs/RESTART/20220720.180000.fv_core.res.nc         ./fv_core.res.nc
 
-#icsdir=./rrfs/INPUT
-#mkdir -p $icsdir
-#cp gfs_ctrl.nc $icsdir/.
-#mv 20220720.180000.fv_core.res.tile1.nc   $icsdir/fv_core.res.tile1.nc
-#mv 20220720.180000.fv_tracer.res.tile1.nc $icsdir/fv_tracer.res.tile1.nc
+# FIX FILES
+halo=".halo4"
+halo=""
+FIX_DIR=/lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs/ufs-srweather-app/regional_workflow/fix/lam/RRFS_CONUS_3km/
+cp $FIX_DIR/C3359_grid.tile7${halo}.nc     ./C3359_grid.tile7${halo}.nc
+cp $FIX_DIR/C3359_oro_data.tile7${halo}.nc ./C3359_oro_data.tile7${halo}.nc
+echo "Copying files.............. Done."
+
+warm=./fv_core.res.tile1.nc
+cold=./out.atm.tile7.nc
+grid=./C3359_grid.tile7${halo}.nc
+akbk=./fv_core.res.nc
+akbkcold=./gfs_ctrl.nc
+orog=./C3359_oro_data.tile7${halo}.nc
+bndy=./gfs.bndy.nc
+
+python da_chgres_winds.py $warm $cold $grid $akbk $akbkcold $orog $bndy
+
+
--- tmp_ICS/src/chgres_winds/chgres_winds.f90
+++ tmp_ICS/src/chgres_winds/chgres_winds.f90
@@ -1,20 +1,24 @@
- subroutine main(geolon_s,geolat_s,geolon_w,geolat_w,u_s,v_s,u_w,v_w,ud,vd)
+ subroutine main(gridx,gridy,u_s,v_s,u_w,v_w,ud,vd)
+ use ISO_FORTRAN_ENV
  use omp_lib
  use, intrinsic :: ieee_arithmetic
  implicit none
- real(kind=8), dimension(:,:),   intent(IN)    :: geolon_s,geolat_s !(nlon,nlat)
+ integer, parameter     :: f_p = selected_real_kind(20)
+ integer, parameter     :: f_d  = 8
  real(kind=8), dimension(:,:,:), intent(IN)    :: u_s,v_s
  real(kind=8), dimension(:,:,:), intent(INOUT) :: ud
- real(kind=8), dimension(:,:),   intent(IN)    :: geolon_w,geolat_w
  real(kind=8), dimension(:,:,:), intent(IN)    :: u_w,v_w
  real(kind=8), dimension(:,:,:), intent(INOUT) :: vd
- real(kind=16) :: inner_prod
+ real(kind=8), dimension(:,:),   intent(IN)    :: gridx,gridy !(nlon,nlat)
+ real(kind=8) :: inner_prod
  real(kind=8), dimension(2):: p1,p2,p3
  real(kind=8), dimension(3):: e1,e2,ex,ey
  integer :: i,j,k
  integer :: is,ie,js,je,ks,ke
  real(kind=8), parameter:: pi=4.D0*DATAN(1.D0)
- real(kind=8), parameter:: deg2rad=pi/180.0
+ real(kind=8), parameter:: deg2rad=pi/180.D0
+ real(kind=8), parameter:: rad2deg=180.D0/pi
+ real(kind=8), parameter:: r360=360.D0
 
  is = lbound(ud, dim=1)
  ie = ubound(ud, dim=1)
@@ -23,61 +27,42 @@
  ks = lbound(ud, dim=3)
  ke = ubound(ud, dim=3)
 
- write(*,*) "is =",is
- write(*,*) "ie =",ie
- write(*,*) "js =",js
- write(*,*) "je =",je
-
- write(*,*) "geolon_s(ie+1,1)",geolon_s(ie+1,1),ie+1
- write(*,*) "geolon_s(1,1)",geolon_s(1,1)
- write(*,*) "deg2rad",deg2rad
-
 !$OMP parallel do default(none) &
-!$OMP          shared(is,ie,js,je,ks,ke,geolon_s,geolat_s,geolon_w,geolat_w,ud,vd,u_s,v_s,u_w,v_w) &
+!$OMP          shared(is,ie,js,je,ks,ke,gridx,gridy,ud,vd,u_s,v_s,u_w,v_w) &
 !$OMP          private(p1,p2,p3,e1,e2,ex,ey)
  do k=ks,ke
-   !write(*,*) "k=",k
-   do j=js,je+1 !lats
+   do j=js,je!+1 !lats
      do i=is,ie !lons
-       p1(1) = (geolon_s(i,  j)-360.0)*deg2rad
-       p1(2) = geolat_s(i,  j)*deg2rad
-       p2(1) = (geolon_s(i+1,j)-360.0)*deg2rad
-       p2(2) = geolat_s(i+1,j)*deg2rad
+       if(i .lt. ie-1 .and. j .le. je) then
+       p1(1) = gridx(i,  j)*deg2rad
+       p1(2) = gridy(i,  j)*deg2rad
+       p2(1) = gridx(i+1,j)*deg2rad
+       p2(2) = gridy(i+1,j)*deg2rad
+       endif
        call mid_pt_sphere(p1, p2, p3)
        call get_unit_vect2(p1, p2, e1)
        call get_latlon_vector(p3, ex, ey)
        ud(i,j,k) = u_s(i,j,k)*inner_prod(e1,ex) + v_s(i,j,k)*inner_prod(e1,ey)
-       if (i .eq. 1 .and. j .eq. 1 .and. k .eq. 1) then
-         write(*,*) "p1=",p1(1),p1(2)
-         write(*,*) "p2=",p2(1),p2(2)
-         write(*,*) "p3=",p3(1),p3(2)
-         write(*,*) "e1=",e1
-         write(*,*) "ex=",ex
-         write(*,*) "ey=",ey
-         write(*,*) "inner_prod(e1,ex)",inner_prod(e1,ex)
-         write(*,*) "inner_prod(e1,ey)",inner_prod(e1,ey)
-         write(*,*) "u_s(i,j,k)",u_s(i,j,k)
-         write(*,*) "v_s(i,j,k)",v_s(i,j,k)
-         write(*,*) "ud(i,j,k)",ud(i,j,k)
-       end if
        if ( isnan(ud(i,j,k))) then
-         write(*,*) "NaN at i,j,k",i,j,k
+         write(*,*) "in ud loop: NaN at i,j,k",i,j,k
          stop
        end if
      enddo
    enddo
    do j=js,je
-     do i=is,ie+1
-       p1(1) = (geolon_w(i,j)-360.0)*deg2rad
-       p1(2) = geolat_w(i,j)*deg2rad
-       p2(1) = (geolon_w(i,j+1)-360.0)*deg2rad
-       p2(2) = geolat_w(i,j+1)*deg2rad
+     do i=is,ie!+1
+       if(i .lt. ie .and. j .le. je-1) then
+       p1(1) = gridx(i,  j)*deg2rad
+       p1(2) = gridy(i,  j)*deg2rad
+       p2(1) = gridx(i,j+1)*deg2rad
+       p2(2) = gridy(i,j+1)*deg2rad
+       endif
        call mid_pt_sphere(p1, p2, p3)
        call get_unit_vect2(p1, p2, e2)
        call get_latlon_vector(p3, ex, ey)
        vd(i,j,k) = u_w(i,j,k)*inner_prod(e2,ex) + v_w(i,j,k)*inner_prod(e2,ey)
        if ( isnan(vd(i,j,k))) then
-         write(*,*) "NaN at i,j,k",i,j,k
+         write(*,*) "in vd loop: NaN at i,j,k",i,j,k
          stop
        end if
      enddo
@@ -87,14 +72,16 @@
  return
  end subroutine main
 
-   real*16 function inner_prod(v1, v2)
+   real*8 function inner_prod(v1, v2)
+       integer, parameter     :: f_p = selected_real_kind(20)
+       integer, parameter     :: f_d  = 8
        real(kind=8),intent(in):: v1(3), v2(3)
-       real(kind=16) :: vp1(3), vp2(3), prod16
+       real(kind=f_p) :: vp1(3), vp2(3), prod16
        integer k
 
          do k=1,3
-            vp1(k) = real(v1(k),kind=16)
-            vp2(k) = real(v2(k),kind=16)
+            vp1(k) = real(v1(k),kind=f_p)
+            vp2(k) = real(v2(k),kind=f_p)
          enddo
          prod16 = vp1(1)*vp2(1) + vp1(2)*vp2(2) + vp1(3)*vp2(3)
          inner_prod = prod16
@@ -102,7 +89,9 @@
   end function inner_prod
 
  subroutine mid_pt_sphere(p1, p2, pm)
+ use ISO_FORTRAN_ENV
  implicit none
+      integer, parameter     :: f_d  = 8
       real(kind=8) , intent(IN)  :: p1(2), p2(2)
       real(kind=8) , intent(OUT) :: pm(2)
 !------------------------------------------
@@ -116,7 +105,9 @@
  end subroutine mid_pt_sphere
 
  subroutine get_unit_vect2( e1, e2, uc )
+ use ISO_FORTRAN_ENV
  implicit none
+   integer, parameter     :: f_d  = 8
    real(kind=8), intent(in) :: e1(2), e2(2)
    real(kind=8), intent(out):: uc(3) !< unit vector e1--->e2
 ! Local:
@@ -134,7 +125,9 @@
  end subroutine get_unit_vect2
 
  subroutine get_latlon_vector(pp, elon, elat)
+ use ISO_FORTRAN_ENV
  implicit none
+ integer, parameter     :: f_d  = 8
  real(kind=8), intent(IN)  :: pp(2)
  real(kind=8), intent(OUT) :: elon(3), elat(3)
 
@@ -154,10 +147,13 @@
 
 !>@brief The subroutine 'normalize_vect' makes 'e' a unit vector.
  subroutine normalize_vect(e)
+ use ISO_FORTRAN_ENV
  implicit none
 
+ integer, parameter     :: f_p = selected_real_kind(20)
+ integer, parameter     :: f_d  = 8
  real(kind=8), intent(inout):: e(3)
- real(kind=16):: pdot
+ real(kind=f_p):: pdot
  integer k
 
     pdot = e(1)**2 + e(2)**2 + e(3)**2
@@ -172,7 +168,9 @@
 !>@brief The subroutine 'vect_cross performs cross products
 !! of 3D vectors: e = P1 X P2
  subroutine vect_cross(e, p1, p2)
+ use ISO_FORTRAN_ENV
  implicit none
+ integer, parameter     :: f_d  = 8
  real(kind=8), intent(in) :: p1(3), p2(3)
  real(kind=8), intent(out):: e(3)
 
@@ -184,15 +182,18 @@
 
 !>@brief The subroutine 'latlon2xyz' maps (lon, lat) to (x,y,z)
  subroutine latlon2xyz(p, e, id)
+ use ISO_FORTRAN_ENV
  implicit none
 
+ integer, parameter     :: f_p = selected_real_kind(20)
+ integer, parameter     :: f_d  = 8
  real(kind=8), intent(in) :: p(2)
  real(kind=8), intent(out):: e(3)
  integer, optional, intent(in):: id !< id=0 do nothing; id=1, right_hand
 
  integer n
- real(kind=16):: q(2)
- real(kind=16):: e1, e2, e3
+ real(kind=f_p):: q(2)
+ real(kind=f_p):: e1, e2, e3
 
  logical, save       :: first_time = .true.
  integer, save       :: id_latlon
@@ -216,12 +217,15 @@
 
 
  subroutine mid_pt3_cart(p1, p2, e)
+ use ISO_FORTRAN_ENV
  implicit none
+       integer, parameter     :: f_p = selected_real_kind(20)
+       integer, parameter     :: f_d  = 8
        real(kind=8), intent(IN)  :: p1(3), p2(3)
        real(kind=8), intent(OUT) :: e(3)
 !
-       real(kind=16):: q1(3), q2(3)
-       real(kind=16):: dd, e1, e2, e3
+       real(kind=f_p):: q1(3), q2(3)
+       real(kind=f_p):: dd, e1, e2, e3
        integer k
 
        do k=1,3
@@ -245,16 +249,19 @@
  end subroutine mid_pt3_cart
 
  subroutine cart_to_latlon(np, q, xs, ys)
+ use ISO_FORTRAN_ENV
  implicit none
 ! vector version of cart_to_latlon1
   integer, intent(in):: np
+  integer, parameter     :: f_p = selected_real_kind(20)
+  integer, parameter     :: f_d  = 8
   real(kind=8), intent(inout):: q(3,np)
   real(kind=8), intent(inout):: xs(np), ys(np)
 ! local
   real(kind=8), parameter:: esl=1.d-10
   real(kind=8), parameter:: pi=4.D0*DATAN(1.D0)
-  real(kind=16):: p(3)
-  real(kind=16):: dist, lat, lon
+  real(kind=f_p):: p(3)
+  real(kind=f_p):: dist, lat, lon
   integer i,k
 
   do i=1,np
@@ -267,12 +274,12 @@
      enddo
 
      if ( (abs(p(1))+abs(p(2)))  < esl ) then
-          lon = real(0.,kind=16)
+          lon = real(0.,kind=f_p)
      else
           lon = atan2( p(2), p(1) )   ! range [-pi,pi]
      endif
 
-     if ( lon < 0.) lon = real(2.,kind=16)*pi + lon
+     if ( lon < 0.) lon = real(2.,kind=f_p)*pi + lon
 ! RIGHT_HAND system:
      lat = asin(p(3))
 
